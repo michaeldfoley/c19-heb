b40426d15000eaa0bebabc2c4f9849260cc923c3
Michael Foley <michael.foley@unqork.com>
2021-04-02 17:56:01 -0400

    Clean up types

--- heb.js
+++ heb.js
@@ -1,20 +1,49 @@
 const axios = require('axios').default
 const geolib = require('geolib')
 const playwright = require('playwright')
-const { sleep, genUserAgent } = require('./utils')
+const { sleep, genUserAgent, noop } = require('./utils')
 
-const noop = () => { }
+const METERS_TO_MILES = 1609.34
+const HEB_GET_SLOTS = 'https://heb-ecom-covid-vaccine.hebdigital-prd.com/vaccine_locations.json'
 
 /**
  * @typedef {'Pfizer' | 'Moderna' | 'Janssen'} VaxTypes
+ * 
+ * @typedef {object} slotDetails
+ * @property {number} openTimeslots
+ * @property {number} openAppointmentSlots
+ * @property {string} manufacturer
+ * 
+ * @typedef {object} Location
+ * @property {string} zip
+ * @property {string} type
+ * @property {string} street
+ * @property {number} storeNumber
+ * @property {string} state
+ * @property {slotDetails[]} slotDetails
+ * @property {number} openTimeslots
+ * @property {number} openAppointmentSlots
+ * @property {string} name
+ * @property {number} [longitude]
+ * @property {number} [latitude]
+ * @property {string} city
+ * 
+ * @typedef {Location & { distance: number }} AppointmentLocation
+ *
+ * @typedef {object} Data
+ * @property {Location[]} locations
  */
 
-/** Check vaccines and once you find one in your area, try to grab it
+/** 
+ * Check vaccines and once you find one in your area, try to grab it
+ * 
  * @param {object} props
  * @param {number | string} props.homeLatitude
  * @param {number | string} props.homeLongitude
  * @param {number} props.maxDistance distance in miles
  * @param {VaxTypes[]} props.types
+ * 
+ * @returns {Promise<boolean>}
  */
 module.exports = async function heb({
   homeLatitude = 30.267153,
@@ -22,32 +51,12 @@ module.exports = async function heb({
   maxDistance = 25,
   types = ['Pfizer', 'Moderna', 'Janssen'],
 } = {}) {
-  const METERS_TO_MILES = 1609.34
-  const HEB_GET_SLOTS = 'https://heb-ecom-covid-vaccine.hebdigital-prd.com/vaccine_locations.json'
 
   console.log(`Searching for ${types.join(', ')} appointments within ${maxDistance} miles of (${homeLatitude}, ${homeLongitude})`)
+
   const result = await axios.get(HEB_GET_SLOTS, {
     headers: { 'User-Agent': genUserAgent() },
   }).catch(console.log)
-
-  /**
-   * @typedef {object} Location
-   * @property {string} zip
-   * @property {string} type
-   * @property {string} street
-   * @property {number} storeNumber
-   * @property {string} state
-   * @property {string[]} slotDetails
-   * @property {number} openTimeslots
-   * @property {number} openAppointmentSlots
-   * @property {string} name
-   * @property {number} [longitude]
-   * @property {number} [latitude]
-   * @property {string} city
-   *
-   * @typedef {object} Data
-   * @property {Location[]} locations
-   */
 
   /** @type {Data} */
   const { locations } = result.data
@@ -57,9 +66,12 @@ module.exports = async function heb({
     return false
   }
 
-  /** @type {Location & { distance: number}} */
-  const availableAppointments = locations.reduce((appointmentLocations, location) => {
-
+  /**
+   * Reduce locations to the ones that have appointments and meet the supplied criteria
+   * @param {AppointmentLocation[]} appointmentLocations 
+   * @param {Location} location
+   */
+  const findAppointments = (appointmentLocations, location) => {
     const {
       latitude,
       longitude,
@@ -70,7 +82,7 @@ module.exports = async function heb({
     if (
       !latitude ||
       !openAppointmentSlots ||
-      !slotDetails.find(slot => types.includes(slot.manufacturer))
+      !slotDetails.some(slot => types.includes(slot.manufacturer))
     ) {
       return appointmentLocations
     }
@@ -83,15 +95,18 @@ module.exports = async function heb({
     return distance <= maxDistance
       ? [...appointmentLocations, { ...location, distance }]
       : appointmentLocations
-  }, [])
+  }
+
+  const availableAppointments = locations.reduce(findAppointments, [])
     .sort((a, b) => a.distance - b.distance)
 
   if (availableAppointments.length) {
+    console.log('Found an appointment!')
     return browser(availableAppointments[0].url)
-    // browser(availableAppointments.map(({ url }) => url))
-  } else {
-    console.log('no appointments found')
   }
+
+  console.log('No appointments found')
+  return new Promise((resolve) => resolve(false))
 }
 
 /**
@@ -101,22 +116,24 @@ module.exports = async function heb({
 async function browser(url) {
   const DATE_SELECTOR = 'input[name="Appointment_Date__c"]'
   const TIME_SELECTOR = 'input[name="Event_Session__c"]'
+  const OPTION_SELECTOR = '[role="option"]'
+  const SUBMIT_BUTTON_SELECTOR = 'lightning-button'
 
   const browser = await playwright.firefox.launch({ headless: false })
   const context = await browser.newContext({ viewport: { width: 1280, height: 800 } })
   let foundAppointment = false
 
   const page = await context.newPage()
+  await page.goto(url)
 
   const closeIfFound = match => value => {
     if (value.trim() === match) {
-      console.log('appointments no longer available')
+      console.log('Appointment no longer available')
       return page.close()
     }
   }
 
-  await page.goto(url)
-
+  /** If slot is already taken, close the page. */
   page.textContent('h1').then(
     closeIfFound('Appointments are no longer available for this location.'),
     noop,
@@ -129,14 +146,18 @@ async function browser(url) {
   await sleep(721)
   await page.click(DATE_SELECTOR).catch(noop)
   const dateSelector = await page.getAttribute(DATE_SELECTOR, 'aria-owns').catch(noop)
-  await page.click(`#${dateSelector} > [role="option"]`).catch(noop)
+  await page.click(`#${dateSelector} > ${OPTION_SELECTOR}`).catch(noop)
 
   await sleep(655)
-  await page.click('input[name="Event_Session__c"]').catch(noop)
+  await page.click(TIME_SELECTOR).catch(noop)
   const timeSelector = await page.getAttribute(TIME_SELECTOR, 'aria-owns').catch(noop)
-  await page.click(`#${timeSelector} > [role="option"]`).catch(noop)
+  await page.click(`#${timeSelector} > ${OPTION_SELECTOR}`).catch(noop)
 
   await sleep(600)
-  await page.click('lightning-button').then(() => foundAppointment = true, () => browser.close())
+  await page.click(SUBMIT_BUTTON_SELECTOR).then(
+    () => foundAppointment = true,
+    () => browser.close()
+  )
+
   return new Promise((resolve) => resolve(foundAppointment))
 }
--- utils.js
+++ utils.js
@@ -4,6 +4,7 @@ const UserAgent = require('user-agents')
  * Resolves the returned promise after the set amount of ms. This should be used with
  * async-await to take advantage of the waiting.
  * @param {number} ms The time in ms to sleep.
+ * @returns {Promise<void>}
  */
 const sleep = (ms) => new Promise((resolve) => {
   setTimeout(resolve, ms)
@@ -18,7 +19,11 @@ const genUserAgent = () => {
   return userAgent.toString()
 }
 
+/** no-op function */
+const noop = () => { }
+
 module.exports = {
   sleep,
   genUserAgent,
+  noop,
 }
